83e2540f14228656e63ddf764bccc968de38b497
janvannimw <jvannimw@gmail.com>
2023-07-12 09:02:51 -0500

    init commit

--- src/main/java/com/jan/javian/controller/Endpoints.java
+++ src/main/java/com/jan/javian/controller/Endpoints.java
@@ -20,7 +20,6 @@ public class Endpoints {
     @GetMapping("/info/contact")
     public JSONPObject getContactInfo(){
         List<Contact> contacts = contactService.findAll();
-
         return contacts == null ? new JSONPObject("contacts", new Contact()) : new JSONPObject("contact", contacts);
     }
 
